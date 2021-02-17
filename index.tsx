6329209dab05b15d311d6404f268ba656a9fb574
wangyouhe.ezio <wangyouhe.ezio@bytedance.com>
2021-02-17 15:16:22 +0800

    feat: 增加异步 setState

--- index.tsx
+++ index.tsx
@@ -2,42 +2,49 @@ import React from "./react/index";
 import ReactDOM from "./reactDom/index";
 
 function Welcome(props) {
-  return <h1>Welcome, {props.name}</h1>;
+    return <h1>Welcome, {props.name}</h1>;
 }
 
 class WelcomeClass extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { time: new Date().toLocaleTimeString(), index: 100 };
-  }
-  componentWillMount() {
-    setInterval(() => {
-      this.setState({ time: new Date().toLocaleTimeString() });
-    }, 10000);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {time: new Date().toLocaleTimeString(), index: 100};
+    }
 
-  handleOnClick() {
-    this.setState({
-      index: this.state.index + 1,
-    });
-  }
-  render() {
-    return (
-      <h1 class="sss">
-        HelloClass <span>{this.props.name}</span>
-        <span> {this.state.time}</span>
-        <br />
-        <button onClick={() => this.handleOnClick()}>{this.state.index}</button>
-        {this.props.children}
-      </h1>
-    );
-  }
+    componentWillMount() {
+        // setInterval(() => {
+        //   this.setState({ time: new Date().toLocaleTimeString() });
+        // }, 10000);
+    }
+
+    handleOnClick() {
+        for (let i = 0; i < 10000; i++) {
+            this.setState((state => {
+                return {
+                    index: state.index + 1
+                }
+            }));
+        }
+
+    }
+
+    render() {
+        return (
+            <h1 class="sss">
+                HelloClass <span>{this.props.name}</span>
+                <span> {this.state.time}</span>
+                <br/>
+                <button onClick={() => this.handleOnClick()}>{this.state.index}</button>
+                {this.props.children}
+            </h1>
+        );
+    }
 }
 
 const app = (
-  <div>
-    <WelcomeClass name="ezio" children={<Welcome name="cracker" />} />
-  </div>
+    <div>
+        <WelcomeClass name="ezio" children={<Welcome name="cracker"/>}/>
+    </div>
 );
 
 ReactDOM.render(app, document.getElementById("root"));
--- react/component.ts
+++ react/component.ts
@@ -1,27 +1,46 @@
-import { renderComponent } from "../reactDom/index";
-import { ReactVNode } from "./index";
+import {renderComponent} from "../reactDom/index";
+import {ReactVNode} from "./index";
 
 type Props = {
-  [k: string]: any;
+    [k: string]: any;
+};
+
+type State = {
+    [k: string]: any;
 };
 
 export default class Component {
-  state: {
-    [k: string]: any;
-  };
-  props: Props;
-  render: () => ReactVNode;
-  componentWillMount: () => void;
-  componentWillReceiveProps: (props: Props) => void;
-  componentWillUpdate: () => void;
-  setState(newState: Props) {
-    this.state = { ...this.state, ...newState };
-    renderComponent(this);
-  }
-  constructor(props = {}) {
-    this.state = {};
-    this.props = props;
-  }
+    state: State;
+    props: Props;
+    render: () => ReactVNode;
+    componentWillMount: () => void;
+    componentWillReceiveProps: (props: Props) => void;
+    componentWillUpdate: () => void;
+    renderFlag = false;
 
-  element: HTMLElement;
+    flush() {
+        this.renderFlag = true;
+        setTimeout(() => {
+            renderComponent(this);
+            this.renderFlag = false
+        })
+    }
+
+    setState(newState: (State) => State | State) {
+        if (typeof newState === "function") {
+            newState = newState(this.state)
+        }
+        this.state = {...this.state, ...newState};
+
+        if (this.renderFlag === false) {
+            this.flush();
+        }
+    }
+
+    constructor(props = {}) {
+        this.state = {} as State;
+        this.props = props;
+    }
+
+    element: HTMLElement;
 }
